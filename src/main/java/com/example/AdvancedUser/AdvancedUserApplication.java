f6539da92267e3ab20a1b1233a1d4800cb90cb45
minister2020 <abdulrasheedidowu86@gmail.com>
2025-11-16 11:26:18 +0000

    change prod url

--- src/main/java/com/example/AdvancedUser/AdvancedUserApplication.java
+++ src/main/java/com/example/AdvancedUser/AdvancedUserApplication.java
@@ -5,10 +5,13 @@ import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Root;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@Cacheable
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.AdvancedUser.repository")
+@EntityScan(basePackages = "com.example.AdvancedUser.model")
 public class AdvancedUserApplication {
 
 	public static void main(String[] args) {
